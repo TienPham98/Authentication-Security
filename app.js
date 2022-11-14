6f8876bf3af431be702743ebfed812a26d77ac96
Pham Nhat Tien <nhattien.ee@gmail.com>
2022-11-14 18:15:30 +0700

    ok

--- app.js
+++ app.js
@@ -181,11 +181,6 @@ app.post("/login", (req, res) => {
 })
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
-
-app.listen(port, () => {
-    console.log("Server has started successfully");
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
